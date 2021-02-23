2fee15046bb893b6cd050c2b7913f226f9952729
Thinh Nguyen <t9ngdu00@students.oamk.fi>
2021-02-23 19:21:45 +0200

    haha

---
+++ components/users.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
+
+let users = [
+    {
+        id: 1,
+        username: 'tester',
+        password: '$2y$06$PhZ74dT8/5g6B8SgssFq6ey4ojLxmP6pos2DcevMUGw25Vc9jGEou', // testerpassword
+    }
+]
+
+router.get('/', (req, res) => {
+    res.status(200).json(users);
+});
+
+//register
+router.post('/register', (req, res) => {
+    if(req.body.username == false || req.body.password == false) {
+        res.status(400);
+        res.json({status: "Please fill all fields!"});
+        return;
+    }
+    else {
+        const username = users.find(u => u.username == req.body.username);
+        if(username) {
+            res.status(500).json({status:"username exist!"});
+        }
+        const hashedPassword = bcrypt.hashSync(req.body.password, 6);
+        const newUser = {
+            id: users.length + 1,
+            username: req.body.username,
+            password: hashedPassword
+        }
+        users.push(newUser);
+        res.status(201).json({status: "created"});
+        // res.send(newUser);
+        // console.log(newUser.username);
+    }
+});
+
+//login
+router.get('/login', (req, res) => {
+    if(req.body.username != false && req.body.password != false) {
+        var username = req.body.username;
+        var password = req.body.password;
+        const user = users.find(u => u.username == username)
+        if(user) {
+            const tam = bcrypt.compareSync(password, user.password);
+            if(tam == false) {
+                res.status(500).json("wrong password");
+            }
+            else {
+                res.status(201).json(user);
+            }
+        }
+        else {
+            res.send("username does not exist!");
+        }
+    }
+    res.status(400);
+    res.json({status: "please fill all  fields!"});
+    return;
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -3,10 +3,41 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var books = require('./routes/books.js');
 var authors = require('./routes/authors.js');
+const userComponent = require('./components/users');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const BasicStrategy = require('passport-http').BasicStrategy;
 var app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
 // You can store key-value pairs in express, here we store the port setting
 app.set('port', (process.env.PORT || 80));
+
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+      const user = users.getUserByName(username);
+      if(user == undefined) {
+        console.log("HTTP Basic username not found");
+        return done(null, false, {message: "HTTP Basic username not found"});
+      }
+      if(bcrypt.compareSync(password, user.password) == false) {
+        console.log("HTTP Basic password not matching username");
+        return done(null, false, {message: "HTTP Basic password not found"});
+      }
+      return done(null, user);
+    }
+  ))
+
+  app.get('/protected',
+  passport.authenticate('basic', {session: false}),
+  (req, res) => {res.json({yourProtectedResource: "profit"});
+});
+
 
 // bodyParser needs to be configured for parsing JSON from HTTP body
 app.use(bodyParser.json());
@@ -14,6 +45,7 @@ app.use(bodyParser.json());
 // Mount our routes behind /api/ prefix
 app.use('/api', books);
 app.use('/api', authors);
+app.use('/products', productComponent );
 
 // Simple hello world route
 app.get('/', function(req, res) {
